bc78853b62749678064dd91d59ea44a5242795d2
faizalajiw <faizalajiwibowo7@gmail.com>
2023-01-13 02:01:58 +0700

    membuat rest api login

---
+++ app/Http/Controllers/Api/Admin/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ]);
+
+        //jika validation gagal
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //jika benar, maka cari user by email dari request "email"
+        $user = User::where('email', $request->email)->first();
+
+        //jika password dari user and password dari request tidak sama
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            
+            //return status code "400" dan login gagal
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Gagal!',
+            ], 400);
+        }
+
+        //return user berhasil login dan create token
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Berhasil!',
+            'user'    => $user,
+            'token'   => $user->createToken('authToken')->accessToken    
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('admin')->group(function () {
+    //route login
+    Route::post('/login', App\Http\Controllers\Api\Admin\LoginController::class, ['as' => 'admin']);
+
+    //group route dengan middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+        //route user logged in
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->name('user');
+    });
 });
