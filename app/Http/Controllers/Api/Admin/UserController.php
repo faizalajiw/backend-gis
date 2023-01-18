7295ed1ea162e2c20a4babbfc6e08ee5809ab6dd
faizalajiw <faizalajiwibowo7@gmail.com>
2023-01-18 11:41:13 +0700

    membuat rest api user admin

---
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Index
+     * menampilkan list user
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::when(request()->q, function ($users) {
+            $users = $users->where('name', 'like', '%' . request()->q . '%');
+        })->latest()->paginate(5);
+
+        return new UserResource(true, 'List Data Users', $users);
+    }
+
+    /**
+     * Store
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //membuat validasi data
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'email'    => 'required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        //jika belum sesuai validasi 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create user
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+
+        if ($user) {
+            //return success with Api Resource
+            return new UserResource(true, 'Data User Berhasil Disimpan!', $user);
+        }
+
+        //return failed with Api Resource
+        return new UserResource(false, 'Data User Gagal Disimpan!', null);
+    }
+
+    /**
+     * Display
+     * menampilkan detail data user
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::whereId($id)->first();
+
+        if ($user) {
+            //return success with Api Resource
+            return new UserResource(true, 'Detail Data User!', $user);
+        }
+
+        //return failed with Api Resource
+        return new UserResource(false, 'Detail Data User Tidak DItemukan!', null);
+    }
+
+    /**
+     * Update
+     * melakukan update data
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //validasi data
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'email'    => 'required|unique:users,email,' . $user->id,
+            'password' => 'confirmed'
+        ]);
+
+        //jika belum sesuai validasi
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //jika sesuai validasi
+        if ($request->password == "") {
+            //update user without password
+            $user->update([
+                'name'      => $request->name,
+                'email'     => $request->email,
+            ]);
+        } else {
+            //update user with new password
+            $user->update([
+                'name'      => $request->name,
+                'email'     => $request->email,
+                'password'  => bcrypt($request->password)
+            ]);
+        }
+
+        if ($user) {
+            //return success with Api Resource
+            return new UserResource(true, 'Data User Berhasil Diupdate!', $user);
+        }
+
+        //return failed with Api Resource
+        return new UserResource(false, 'Data User Gagal Diupdate!', null);
+    }
+
+    /**
+     * Remove
+     * menghapus data
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        if ($user->delete()) {
+            //return success with Api Resource
+            return new UserResource(true, 'Data User Berhasil Dihapus!', null);
+        }
+
+        //return failed with Api Resource
+        return new UserResource(false, 'Data User Gagal Dihapus!', null);
+    }
+}
